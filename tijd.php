36d0ab71eec2bc71d334ca3e8f17ddebbb580319
2099492 <2099492@talnet.com>
2019-11-29 14:25:34 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -1,23 +1,44 @@
 <?php
-$string = readline();
-$array = array();
-$array[] = $string;
+$a = explode(" ", $argv[1]);
+$eind = 0;
+// for ($i=0; $i <4 ; $i++) {
+//   $argv[1]++;
+// }
 
-$day = d;
-$sec = s;
-$min = m;
-$hour = h;
+foreach ($a as $value) {
 
-$pos = strpos($string);
+$last = substr($value, -1);
 
-switch ($string) {
-  case $day:
-        echo $string * 86400;
+  switch ($last) {
 
+    case "d":
+      $sec=(int)$value*86400;
+      $eind += $sec;
+      break;
+
+      case "h":
+        $sec=(int)$value*3600;
+        $eind += $sec;
+        break;
+
+        case "m":
+          $sec=(int)$value*60;
+          $eind += $sec;
+          break;
+
+
+      case "s":
+        $sec=$sec+(int)$value;
+        $eind += $sec;
+              break;
+
+    default:
+    echo " is geen getal";
     break;
-
-  default:
-    echo $string " is geen tijd";
-    break;
+  }
 }
+
+print_r($sec);
+print_r(" seconden")
+
  ?>
